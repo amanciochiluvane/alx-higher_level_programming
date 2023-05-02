b9e3b508c3e7b06aaa3366d2c474f95a2d9ccbeb
AmâncioChiluvane <109949333+amanciochiluvane@users.noreply.github.com>
2023-05-02 15:50:02 +0200

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
-# Takes a URL, sends a request and displays the size of the body
-curl -sI $1 | grep -i content-length | awk '{print $2}'
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <url>"
+    exit 1
+fi
+
+url="$1"
+
+response=$(curl -s "$url")  # Get the response body only
+content_length=$(python -c "import sys; print(len(sys.stdin.read()))" <<< "$response")
+
+if [ -z "$content_length" ]; then
+    echo "Unable to get content length"
+    exit 1
+fi
+
+echo "Size of response body: $content_length bytes"
+
