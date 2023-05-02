fac8b8983a3b81bfd3acd325a7ab1880b1e1bd75
AmâncioChiluvane <109949333+amanciochiluvane@users.noreply.github.com>
2023-05-02 15:54:01 +0200

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
-
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <url>"
-    exit 1
-fi
-
-url="$1"
-
-response=$(curl -sI "$url")  # Get headers only
-content_length=$(echo "$response" | grep -i Content-Length | awk '{print $2}')
-
-if [ -z "$content_length" ]; then
-    echo "Unable to get content length"
-    exit 1
-fi
-
-echo "Size of response body: $content_length bytes"
+# Takes a URL, sends a request and displays the size of the body
+curl -sI $1 | grep -i content-length | awk '{print $2}'
 
 
